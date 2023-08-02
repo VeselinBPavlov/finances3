75ee75e5ffe0bad3f9c2d72ddea813cb5f9b5c7b
RISAK\veselin <veselin.b.pavlov@gmail.com>
2023-08-02 12:32:51 +0300

    Added Expense Categories.

---
+++ Finances/Finances/Services/ExpenseCategories/Commands/Delete/DeleteExpenseCategoryCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Finances.Common.Exceptions;
+using Finances.Common.Interfaces;
+using Finances.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Application.Expenses.Commands.Delete
+{
+    public class DeleteExpenseCategoryCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeleteExpenseCategoryCommandHandler : IRequestHandler<DeleteExpenseCategoryCommand, bool>
+    {
+        private readonly IApplicationDbContext context;
+
+        public DeleteExpenseCategoryCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> Handle(DeleteExpenseCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var expenseCategory = await this.context.ExpenseCategories
+                .Include(ec => ec.Expenses)
+                .SingleOrDefaultAsync(ec => ec.Id == request.Id);
+
+            if (expenseCategory == null)
+            {
+                throw new NotFoundException(nameof(ExpenseCategory), request.Id);
+            }
+
+            this.context.ExpenseCategories.Remove(expenseCategory);
+            await this.context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
---
+++ Finances/Finances/Services/ExpenseCategories/Queries/GetExpensesByCategory/ExpenseByCategoryVm.cs
@@ -0,0 +1,15 @@
+﻿namespace Finance.Application.ExpenseCategories.Queries.GetExpensesByCategory
+{
+    public class ExpenseByCategoryVm
+    {
+        public int Id { get; set; } = default!;
+
+        public string Name { get; set; } = default!;
+
+        public int TypeId { get; set; }
+
+        public string TypeDescription { get; set; } = default!;
+
+        public decimal Sum { get; set; }
+    }
+}
