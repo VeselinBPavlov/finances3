831ac5b2825239df07a7a6f7a2ca0f25fb369c96
RISAK\veselin <veselin.b.pavlov@gmail.com>
2023-08-02 06:05:36 +0300

    Fixed issues.

--- Finances/Finances/Program.cs
+++ Finances/Finances/Program.cs
@@ -11,9 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
